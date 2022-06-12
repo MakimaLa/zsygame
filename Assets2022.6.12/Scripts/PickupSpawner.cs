190918a23432661755f4b02b80f77974962c2f6a
MakimaLa <95695973+MakimaLa@users.noreply.github.com>
2022-06-12 21:01:24 +0800

    2022.6.12
    此版本新增背景的bus，car 、swan、river完整的动画，以及开始暂停和调节音量的UI

---
+++ Assets2022.6.12/Scripts/PickupSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpanwer : MonoBehaviour
+{
+
+	public GameObject[] pickups;
+	public float pickupDeliveryTime = 5f;
+	public float dropRangeleft;
+	public float dropRangeright;
+	public float highHealthThreshold = 75f;
+	public float lowHealthThreshold = 25f;
+
+	private PlayerHealth playerHealth;
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+
+	}
+	void Start()
+	{
+		StartCoroutine(DeliverPickup());
+	}
+
+	public IEnumerator DeliverPickup()
+	{
+		// 等待投放延迟
+		yield return new WaitForSeconds(pickupDeliveryTime);
+
+		float dropPosX = Random.Range(dropRangeleft, dropRangeright);
+
+		Vector3 dropPos = new Vector3(dropPosX, 15f, 1f);
+
+		if (playerHealth.health >= highHealthThreshold)
+			Instantiate(pickups[0], dropPos, Quaternion.identity);
+
+		else if (playerHealth.health <= lowHealthThreshold)
+			Instantiate(pickups[1], dropPos, Quaternion.identity);
+
+		else
+		{
+			int pickupIndex = Random.Range(0, pickups.Length);
+			Instantiate(pickups[pickupIndex], dropPos, Quaternion.identity);
+		}
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
